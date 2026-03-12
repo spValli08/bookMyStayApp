4882eacc11773bd100f81c213e3173f2af846e00
spValli08 <sp1568@srmist.edu.in>
2026-03-12 10:01:39 +0530

    UC1:Welcome Message

---
+++ src/stayApp.java
@@ -0,0 +1,30 @@
+/**
+ * UseCase1HotelBookingApp.java
+ *
+ * This class serves as the entry point for the Hotel Booking Management System.
+ * It demonstrates how a Java application starts execution and prints a welcome message.
+ *
+ * Key Concepts:
+ * - Class definition
+ * - main() method as the entry point
+ * - static keyword
+ * - Console output using System.out.println()
+ * - JavaDoc comments
+ *
+ * @author Sreevalli Pallapothu
+ * @version 1.0
+ */
+public class stayApp {
+
+    /**
+     * Main method - Entry point of the application.
+     * JVM starts execution from this method.
+     *
+     * @param args Command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay App!");
+        System.out.println("Hotel Booking Management System v1.0");
+        System.out.println("Application has started successfully.");
+    }
+}
